650b0ff8c43362c3d6de75a5aa1dfe2ddac28e14
Jerome Morales <194875286+morales-lc@users.noreply.github.com>
2025-04-21 00:45:32 +0800

    change username/name and password functionality, also user list with filter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,7 @@
     <title>Dashboard</title>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+
     <style>
 
     </style>
@@ -15,10 +16,13 @@
     @include('nav')
 
     <div class="container">
-        <h2>Welcome to Your Dashboard</h2>
+        <h2>Welcome to Your Dashboard,  {{ session('user')->first_name }}!</h2>
         <p>Here is where you can manage your account and settings.</p>
     </div>
 
 
 </body>
 </html>
+
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
